f65ae490fca067dd1f75ba11f144ae0462655940
Ramkumar-21-gk <gkramkumarprajapati49@gmail.com>
2026-01-14 08:07:47 +0530

    Terraform use of datasoruce in aws v:2

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ provider "aws" {
   region = var.region
 }
 
+# Data Source to get the most recent Amazon Linux AMI
 data "aws_ami" "name" {
   most_recent = true
   owners      = ["amazon"]
@@ -19,6 +20,56 @@ data "aws_ami" "name" {
 output "ami_id" {
   description = "ami id getting through datasource"
   value = data.aws_ami.name.id
+}
+
+# Data Source to get VPC by tag Name
+data "aws_vpc" "my-vpc" {
+  tags = {
+    Name = "my-vpc"
+  }
+}
+output "vpc_id" {
+  description = "aws vpc id"
+  value = data.aws_vpc.my-vpc.id
+}
+
+# Data Source to get Security Group by tag Name
+data "aws_security_group" "my-security-group" {
+  tags = {
+    Name = "nginx-sg"
+  }
+}
+output "security_group_id" {
+  description = "aws security group id"
+  value = data.aws_security_group.my-security-group.id 
+}
+
+# Data Source to get all available availability zones
+data "aws_availability_zones" "zones" {
+  state = "available"
+}
+
+output "aws_zones" {
+  description = "aws availability zones"
+  value = data.aws_availability_zones.zones.names
+}
+
+# Data Source to get current region
+data "aws_region" "my-region" {
+
+}
+output "current_region" {
+  description = "current aws region"
+  value = data.aws_region.my-region.region  
+}
+
+# Data Source to get caller identity
+data "aws_caller_identity" "current" {
+}
+
+output "caller_info" {
+  description = "value"
+  value = data.aws_caller_identity.current
 }
 
 resource "aws_instance" "my_server" {
